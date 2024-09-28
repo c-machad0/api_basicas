e8c3a7df29bebd78d3a21efebf5c04bcaaf0e773
Christian Machado <realchris.machado@gmail.com>
2024-09-28 15:46:39 -0300

    Atualizando o código as melhoras dos métodos PUT e DELETE. Adicionando também, uma função para retornar, sempre que for pedido, o ID da tarefa

--- to-do-list_app.py
+++ to-do-list_app.py
@@ -8,6 +8,9 @@ tasks = [
     'description': 'Ir para academia'
     }
 ]
+# Função que retorna o ID solicitado e assim que é encontrado, a iteração para
+def get_task_by_id(task_id):
+    return next((task for task in tasks if task['id'] == task_id), None)
 
 @app.route('/')
 def home():
@@ -21,7 +24,7 @@ def get_tasks():
 def create_tasks():
     # Verifica se a requisição está em JSON
     if not request.is_json:
-        return jsonify({'message': 'Requisição deve ser em JSON'})
+        return jsonify({'message': 'Requisição deve ser em JSON'}), 400
 
     request_task = request.get_json()
     description = request_task.get('description')
@@ -32,7 +35,7 @@ def create_tasks():
     
     # Verifica se ja existe a descrição
     if any(task['description'] == description for task in tasks):
-        return jsonify({'message': f'A tarefa {description} já existe.'})
+        return jsonify({'message': f'A tarefa {description} já existe.'}), 400
 
     # Verifica o maior numero de id existente e incrementa 1, gerando o proximo id
     new_id = max([task['id'] for task in tasks], default=0) + 1
@@ -43,24 +46,37 @@ def create_tasks():
         
     tasks.append(new_task)
 
-    return jsonify(new_task), 200
+    return jsonify(new_task), 201
 
 @app.route('/tasks/<int:id>', methods=['PUT'])
 def update_task(id):
+    task = get_task_by_id(id)
+    if not task:
+        return jsonify({'message': f'Tarefa {id} não encontrada.'}), 404
+    
+    if not request.is_json:
+            return jsonify({'message': 'Descrição é obritatória e deve ser maior que 3 caracteres'}), 400
+
     request_task = request.get_json()
-    for task in tasks:
-        if task['id'] == id:
-            task['description'] = request_task.get('description', task['description'])
-            return jsonify(task), 200
-        
-    return jsonify(f'message: Tarefa {id} não encontrada'), 404
+    description = request_task.get('description')
+
+    if description:
+        if len(description) < 3:
+            return jsonify({'message': 'Descrição é obritatória e deve ser maior que 3 caracteres'}), 400
+        # Verifica se existe a mesma descrição dentre todas as tarefas
+        if any(t['description'] == description and t['id'] != id for t in tasks):
+            return jsonify({'message': f'A descrição {description} já está em uso.'}), 400
+        task['description'] = description
+    
+    return jsonify(task), 200
 
 @app.route('/tasks/<int:id>', methods=['DELETE'])
 def delete_task(id):
-    for task in tasks:
-        if task['id'] == id:
-            tasks.remove(task)
-            return jsonify(f'Tarefa {id} foi removida'), 200
-    return jsonify(f'message: Tarefa {id} não encontrada'), 404
+    task = get_task_by_id(id)
+    if not task:
+        return jsonify({'message': f'Tarefa {id} não encontrada.'}), 404
+    
+    tasks.remove(task)
+    return jsonify({'message': f'Tarefa {id} foi removida.'}), 200
 
 app.run(port=5000)
