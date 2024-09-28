a7eeaae1bef263431c4120426fa3fdb87073aa2a
Christian Machado <realchris.machado@gmail.com>
2024-09-28 11:49:54 -0300

    Adicionando os metodos PUT e DELETE

--- to-do-list_app.py
+++ to-do-list_app.py
@@ -29,4 +29,22 @@ def create_tasks():
 
     return jsonify(task)
 
-app.run(port=5000) # inicializa a api
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    request_task = request.get_json()
+    for task in tasks:
+        if task['id'] == id:
+            task['description'] = request_task.get('description', task['description'])
+            return jsonify(task), 200
+        
+    return jsonify(f'message: Tarefa {id} não encontrada'), 404
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    for task in tasks:
+        if task['id'] == id:
+            tasks.remove(task)
+            return jsonify(f'Tarefa {id} foi removida'), 200
+    return jsonify(f'message: Tarefa {id} não encontrada'), 404
+
+app.run(port=5000)
