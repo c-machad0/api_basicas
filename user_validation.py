b165857b60fdfdb48cca82d6805f5352e73a176e
Christian Machado <realchris.machado@gmail.com>
2024-10-12 12:42:15 -0300

    Adicionando métodos PUT e DELETE

--- user_validation.py
+++ user_validation.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 users = [
     {
     'id': 1,
-    'nome': 'Christian',
-    'email': 'realc.machado@hotmail.com'
+    'nome': 'Luiz',
+    'email': 'luizdev123@hotmail.com'
     }
 ]
 
@@ -21,15 +21,34 @@ def get_user():
 @app.route('/users', methods=['POST'])
 def create_user():
     request_user = request.get_json()
-    user = {
+    new_user = {
         'id': request_user['id'],
         'nome': request_user['nome'],
         'email': request_user['email']
     }
 
-    users.append(user)
+    users.append(new_user)
 
-    return jsonify(user), 201
+    return jsonify(new_user), 201
 
+@app.route('/users/<int:id_user>', methods=['PUT'])
+def update_user(id_user):
+    request_user = request.get_json()
 
+    for user in users:
+        if user['id'] == id_user:
+            user['nome'] = request_user.get('nome', user['nome'])
+            user['email'] = request_user.get('email', user['email'])
+            return jsonify(user), 200
+
+    return jsonify({'message': f'Usuário {id_user} não encontrado'}), 404   
+
+@app.route('/users/<int:id_user>', methods=['DELETE'])
+def delete_user(id_user):
+    for user in users:
+        if user['id'] == id_user:
+            users.remove(user)
+            return jsonify({'message': f'Usuario {id_user} foi removido com sucesso'}), 200
+    
+    return jsonify({'message': f'Usuario {id_user} não foi encontrado'})    
 app.run(port=5000)
